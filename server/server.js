282abd26ef54a36ea1d9f8d3b27f56ba6af90d92
Adeel <adeelh17@gmail.com>
2026-02-25 12:32:39 +0000

    Add React build serving for render deployment

--- server/server.js
+++ server/server.js
@@ -157,4 +157,9 @@ app.post("/api/financials/expenses", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
+const path = require("path");
+app.use(express.static(path.join(__dirname, "../client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
